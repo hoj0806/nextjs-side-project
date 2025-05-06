80e78e024e57aa7e399a5ccd01dcd510e8f73110
홍재훈 <ehswkfn123@naver.com>
2025-05-06 20:01:13 +0900

    feature : github 소셜 로그인 구현

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { signInWithGithub } from "../actions";
+
 const LoginPage = () => {
-  return <>Login page</>;
+  return (
+    <>
+      <h1>Login page</h1>
+      <form>
+        <button formAction={signInWithGithub}>깃허브 로그인</button>
+      </form>
+    </>
+  );
 };
 
 export default LoginPage;
