dc3fa882f59f785656575994668ed7a2c9de7680
홍재훈 <ehswkfn123@naver.com>
2025-05-06 21:24:26 +0900

    feature : 카카오 로그인 구현

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,55 @@
 "use client";
 
-import { signInWithGithub } from "../actions";
+import { createClient } from "@/utils/supabase/client";
 
 const LoginPage = () => {
+  const signInWithGithub = async () => {
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      console.error("OAuth 로그인 에러:", error.message);
+    }
+  };
+
+  const signInWithKakao = async () => {
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      console.error("OAuth 로그인 에러:", error.message);
+    }
+  };
+
   return (
     <>
       <h1>Login page</h1>
-      <button onClick={signInWithGithub}>깃허브 로그인</button>
+      <form>
+        <button
+          onClick={signInWithGithub}
+          className='bg-gray-400 p-2 rounded-lg'
+        >
+          깃허브 로그인
+        </button>
+        <button
+          onClick={signInWithKakao}
+          className='bg-yellow-300 p-2 rounded-lg text-black'
+        >
+          카카오 로그인
+        </button>
+      </form>
     </>
   );
 };
