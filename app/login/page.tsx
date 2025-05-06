b40d3d8985dff908058a776f2778c6966efa7db1
홍재훈 <ehswkfn123@naver.com>
2025-05-06 21:42:23 +0900

    feature : 구글 로그인 구현

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -33,6 +33,21 @@ const LoginPage = () => {
     }
   };
 
+  const signInWithGoogle = async () => {
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      console.error("OAuth 로그인 에러:", error.message);
+    }
+  };
+
   return (
     <>
       <h1>Login page</h1>
@@ -48,6 +63,12 @@ const LoginPage = () => {
           className='bg-yellow-300 p-2 rounded-lg text-black'
         >
           카카오 로그인
+        </button>
+        <button
+          onClick={signInWithGoogle}
+          className='bg-white p-2 rounded-lg text-black'
+        >
+          구글 로그인
         </button>
       </form>
     </>
