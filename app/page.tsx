a454a848a67af536f34fa6a245693a9f801fdc1f
홍재훈 <ehswkfn123@naver.com>
2025-05-09 04:26:12 +0900

    feature : 검색 기능 구현

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import PostGrid from "@/components/study/post-grid";
 export default async function Home({
   searchParams,
 }: {
-  searchParams: { category?: string; mode?: string; position?: string };
+  searchParams: {
+    category?: string;
+    mode?: string;
+    position?: string;
+    search?: string;
+  };
 }) {
   return <PostGrid searchParams={searchParams} />;
 }
