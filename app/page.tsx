ebdfca053fafbf1bde668491bd176c84efeb5964
홍재훈 <ehswkfn123@naver.com>
2025-05-09 07:18:09 +0900

    feature : 게시물 페이지 네이션 기능 구현

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home({
     mode?: string;
     position?: string;
     search?: string;
+    page?: string;
   };
 }) {
   return <PostGrid searchParams={searchParams} />;
