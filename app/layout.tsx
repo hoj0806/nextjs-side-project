27f4c92d108f91bcd361225fb3458d4050130bcd
홍재훈 <ehswkfn123@naver.com>
2025-05-08 23:43:36 +0900

    style : rootLayout 너비 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,24 +8,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='ko'>
       <body>
         <main className='min-h-screen flex flex-col items-center'>
-          <nav className='w-full flex justify-center border-b h-16'>
-            <div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
-              <div className='flex gap-5 items-center font-semibold'>
-                <Link href='/'>Home</Link>
-                <Link href='/register'>팀원 모집하기</Link>
+          <div className='w-full max-w-screen-xl'>
+            <nav className='w-full flex justify-center border-b h-16'>
+              <div className='w-full flex justify-between items-center p-3 px-5 text-sm'>
+                <div className='flex gap-5 items-center font-semibold'>
+                  <Link href='/'>Home</Link>
+                  <Link href='/register'>팀원 모집하기</Link>
+                </div>
+                <HeaderAuth />
               </div>
-              <HeaderAuth />
-            </div>
-          </nav>
-
-          <div className='flex flex-col gap-20 max-w-5xl p-5'>{children}</div>
-
-          <footer className='w-full flex items-center justify-center border-t text-xs py-16'>
-            <p>Holla</p>
-          </footer>
+            </nav>
+            <div className='p-5'>{children}</div>
+          </div>
         </main>
       </body>
     </html>
